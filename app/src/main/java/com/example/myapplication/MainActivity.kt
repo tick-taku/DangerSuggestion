2126b9c467d36055df09bf978196de4b5ac0b8d0
tick-taku <39234775+tick-taku@users.noreply.github.com>
2023-04-13 19:53:27 +0900

    Update app/src/main/java/com/example/myapplication/MainActivity.kt
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController
+        setupActionBarWithNavController(navController, appBarConfiguration)
             , appBarConfiguration)
 
         binding.fab.setOnClickListener { view ->
