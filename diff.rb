3a331a7438720148826b5d48beeef8c509c95657
tick-taku <tick.taku.77@gmail.com>
2023-04-13 20:03:56 +0900

    Target multiple lines

--- diff.rb
+++ diff.rb
@@ -5,8 +5,9 @@ diffs = ::GitDiff.from_string(`git diff --unified=0 HEAD`).files.map { |file|
     addition_lines = hunk.lines.map do |line|
       line.content.scan(/^\+([^+].*)/)
     end
-    target_line = hunk.range_info.original_range.start
-    {'code': addition_lines.join('\n'), 'target': target_line}
+    target_range = hunk.range_info.original_range
+    target_line = "#{target_range.start}-#{target_range.start + target_range.number_of_lines}"
+    {'code': addition_lines.select{|line| !line.empty?}.join("\n"), 'target': target_line}
   }
   {'file_path': file.b_path, 'hunks': hunks}
 }
